39220b0e0cf896371af8ef4622bfbfe92064e77f
Thomas Taege <thomas.taege@gmail.com>
2014-03-21 10:43:30 +1000

    added mailer templates for leave requests and toil requests

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,15 @@ class UserMailer < ActionMailer::Base
     @url = 'http://enerleave.eneraque.com/login'
     mail(to: @user.email, subject: 'Welcome to EnerLeave: Leave Tracking')
   end
+
+  def leave_request(user)
+    @user = user
+    @url = 'http://enerleave.eneraque.com/leave_requests/approvals'
+    mail(to: enerleaveadmins@eneraque.com, subject: 'New EnerLeave Leave Request')
+  end
+
+  def toil_request(user)
+    @user = user
+    @url = "http://enerleave.eneraque.com/users/#{@user.id}/toil"
+    mail(to: enerleaveadmins@eneraque.com, subject: 'New EnerLeave Toil Request')
 end
