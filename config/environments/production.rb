d7fa60a66251b3e21b1d154cbdbc3c922641abf5
Thomas Taege <thomas.taege@gmail.com>
2014-03-21 10:11:44 +1000

    added basic actionmailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,12 @@ Enerleave::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Basic Actionmailer Config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        'mail.eneraque.com',
+    port:           25,
+    domain:         'eneraque.com'
+  }
 end
