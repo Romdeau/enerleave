2616c71d6fb89a2c063589ccaf8ffe2241fb8ade
Thomas Taege <thomas.taege@gmail.com>
2014-03-28 14:49:18 +1000

    fixed environment variable error for reset mailer and change redirect route to login form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Enerleave::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'enerleave.eneraque.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
