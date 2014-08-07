9a2bccc05f4d66bc0416b5338c1c9cfde6e556cd
Thomas Taege <thomas.taege@gmail.com>
2014-08-07 10:30:33 +1000

    fixed homepage display issue

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,5 @@
 class HomepageController < ApplicationController
   def index
-    @leave_requests = LeaveRequest.approved_leave.reorder("end_date DESC").page(params[:page]).per_page(8)
+    @leave_requests = LeaveRequest.approved_leave.reorder("end_date DESC")#.page(params[:page]).per_page(8)
   end
 end
